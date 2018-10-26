7397d9727493dc58455d7cd65df8fc5ed90983de
brianhouse <brian.house@gmail.com>
2018-10-25 20:02:00 -0400

    full path

--- upload.py
+++ upload.py
@@ -7,5 +7,5 @@ s3 = boto3.resource('s3')
 # for bucket in s3.buckets.all():
 #     print(bucket.name)
 
-data = open("backup.tar.gz", 'rb')
+data = open("/Users/house/now_here/backup.tar.gz", 'rb')
 s3.Bucket('now-here-backup').put_object(Key="backup.tar.gz", Body=data)
